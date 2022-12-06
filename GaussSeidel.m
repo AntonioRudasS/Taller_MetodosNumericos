9ae59394d5d2518fd4651d6273b60a62177b6a23
Antonio José Rudas Sanchez <62669845+AntonioRudasS@users.noreply.github.com>
2022-12-06 10:17:02 -0500

    Add files via upload

--- GaussSeidel.m
+++ GaussSeidel.m
@@ -11,7 +11,7 @@ for k = 1:m
               return
          end
          
-         fprintf('\n Solucion: %4.0f\n', k)
+         fprintf('\n la solucion del sistema en la iteración %4.0f\n', k)
          for i = 1:n
              fprintf('             x(%1.0f)=%6.8f\n', i, x(i))       
          end
